eca561be877d6c77f364294630379d7d2de00f12
jalil <wnxhaja@gmail.com>
2024-04-19 22:24:39 +0800

    mutations

--- client/src/pages/CompanyPage.js
+++ client/src/pages/CompanyPage.js
@@ -6,14 +6,31 @@ import JobList from '../components/JobList';
 
 function CompanyPage() {
   const { companyId } = useParams();
-  const [company, setCompany] = useState()
+  const [state, setState] = useState({
+    company: null,
+    loading: true,
+    error: false,
+  });
 
   useEffect(() => {
-    getCompany(companyId).then(setCompany)
+    (async () => {
+      try{
+        const company = await getCompany(companyId);
+        setState({ company, loading: false, error: false })
+      }catch(err){
+        setState({ company: null, loading: false, error: true })
+      }
+    })()
   },[companyId])
 
-  if(!company){
+  const { company, loading, error } = state;
+
+  if(loading){
     return  <div>Loading...</div>
+  }
+
+  if(error){
+    return <div className='has-text-dagger'>Data unavailable</div>
   }
 
   return (
--- server/resolvers.js
+++ server/resolvers.js
@@ -1,11 +1,33 @@
+import { GraphQLError }from 'graphql';
 import { getCompany } from './db/companies.js';
-import { getJobs, getJob, getJobsByCompany } from './db/jobs.js';
+import { createJob, deleteJob, getJobs, getJob, getJobsByCompany, updateJob } from './db/jobs.js';
 
 export const resolvers = {
   Query: {
-    company: (_root, {id}) => getCompany(id),
-    job: (_root, {id}) => getJob(id),
+    company: async (_root, { id }) => {
+      const company = await getCompany(id);
+      if (!company) {
+        throw notFoundError('No Company found with id ' + id);
+      }
+      return company;
+    },
+    job: async (_root, { id }) => {
+      const job = await getJob(id);
+      if (!job) {
+        throw notFoundError('No Job found with id ' + id);
+      }
+      return job;
+    },
     jobs: () => getJobs(),
+  },
+
+  Mutation: {
+    createJob: (_root, { input: { title, description } }) => {
+      const companyId = 'FjcJCHJALA4i'; // TODO set based on user
+      return createJob({ companyId, title, description });
+    },
+    deleteJob: (_root, { id }) => deleteJob(id),
+    updateJob: (_root, { input }) => updateJob(input)
   },
 
   Company: {
@@ -17,6 +39,12 @@ export const resolvers = {
     date: (job) => toIsoDate(job.createdAt),
   },
 };
+
+function notFoundError(message) {
+  return new GraphQLError(message, {
+    extensions: { code: 'NOT_FOUND' },
+  });
+}
 
 function toIsoDate(value) {
   return value.slice(0, 'yyyy-mm-dd'.length);
